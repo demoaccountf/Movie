8690e6ea706fdfb3e30486f2300f0d593cc21491
isomer04 <isomer008@gmail.com>
2023-05-10 11:44:15 -0400

    Added Actors, Cinemas, Producers, and Movies controller

---
+++ onlineTickets/Controllers/CinemasController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using onlineTickets.Data;
+
+namespace onlineTickets.Controllers
+{
+    public class CinemasController : Controller
+    {
+
+        private readonly AppDbContext _context;
+
+        public CinemasController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var allProducts = await _context.Cinemas.ToListAsync();
+            return View();
+        }
+    }
+}
---
+++ onlineTickets/Controllers/MoviesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace onlineTickets.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}

3472599a0c5ecc2a447e8a6e13d80e2f0e7128c7
isomer04 <isomer008@gmail.com>
2023-05-12 23:13:50 -0400

    Added IActorService Interface

---
+++ onlineTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,15 @@
+﻿using onlineTickets.Models;
+
+namespace onlineTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        IEnumerable<Actor> GetAll();
+
+        Actor GetByID(int id);
+
+        void Add(Actor actor);
+        Actor Update(int id, Actor newActor);
+        void Delete(int id);
+    }
+}

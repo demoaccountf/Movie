c6f5705c28a10377c75e08b695e92dbec5c07faf
isomer04 <isomer008@gmail.com>
2023-05-25 22:13:22 -0400

    Fixed the Add New Actor error

--- onlineTickets/Models/Actor.cs
+++ onlineTickets/Models/Actor.cs
@@ -23,7 +23,7 @@ namespace onlineTickets.Models
         public string Bio { get; set; }
 
         //Relationships
-        public List<Actor_Movie> Actors_Movies { get; set; }
+        public List<Actor_Movie>? Actors_Movies { get; set; }
 
     }
 }

91704450299cc271503b21962daf3b5938b18a2f
isomer04 <isomer008@gmail.com>
2023-05-29 11:46:38 -0400

    Deleted data from data from database( Delete actor)

--- onlineTickets/Controllers/ActorsController.cs
+++ onlineTickets/Controllers/ActorsController.cs
@@ -42,19 +42,16 @@ namespace onlineTickets.Controllers
         public async Task<IActionResult> Details(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
-
             if (actorDetails == null) return View("NotFound");
             return View(actorDetails);
         }
 
 
-        // Get: Actors/Edit
+        // Get: Actors/Edit/1
         public async Task<IActionResult> Edit(int id)
         {
             var actorDetails = await _service.GetByIdAsync(id);
-
             if (actorDetails == null) return View("NotFound");
-
             return View(actorDetails);
         }
 
@@ -70,5 +67,22 @@ namespace onlineTickets.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        //Get: Actors/Delete/1
+        public async Task<IActionResult> Delete(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+            return View(actorDetails);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var actorDetails = await _service.GetByIdAsync(id);
+            if (actorDetails == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }

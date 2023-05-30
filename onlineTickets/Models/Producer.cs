8e7ced92dd451cd2b4e975ebd6cbe282686ad93c
isomer04 <isomer008@gmail.com>
2023-05-30 00:32:43 -0400

    Added producer details and a new producer to the database.

--- onlineTickets/Models/Producer.cs
+++ onlineTickets/Models/Producer.cs
@@ -3,24 +3,26 @@ using System.ComponentModel.DataAnnotations;
 
 namespace onlineTickets.Models
 {
-    public class Producer: IEntityBase
+
+    public class Producer : IEntityBase
     {
         [Key]
         public int id { get; set; }
 
         [Display(Name = "Profile Picture")]
+        [Required(ErrorMessage = "Profile Picture is required")]
         public string ProfilePictureURL { get; set; }
 
         [Display(Name = "Full Name")]
-
+        [Required(ErrorMessage = "Full Name is required")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Full Name must be between 3 and 50 chars")]
         public string FullName { get; set; }
 
         [Display(Name = "Biography")]
-
+        [Required(ErrorMessage = "Biography is required")]
         public string Bio { get; set; }
 
         //Relationships
         public List<Movie> Movies { get; set; }
-
     }
 }

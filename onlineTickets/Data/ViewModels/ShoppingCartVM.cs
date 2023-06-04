9902450c3b7937d6318a67ef0e2d370cfd7cfcdd
isomer04 <isomer008@gmail.com>
2023-06-03 22:36:32 -0400

    Added OrdersController file

---
+++ onlineTickets/Data/ViewModels/ShoppingCartVM.cs
@@ -0,0 +1,15 @@
+﻿using onlineTickets.Data.Cart;
+
+namespace onlineTickets.Data.ViewModels
+{
+    public class ShoppingCartVM
+    {
+        public ShoppingCart ShoppingCart { get; set; }
+        public double ShoppingCartTotal { get; set; }
+
+        public static implicit operator ShoppingCartVM(ShoppingCartVM v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

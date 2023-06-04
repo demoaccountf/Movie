8779bc49b59de0a14376feedf1b572c4b380435b
isomer04 <isomer008@gmail.com>
2023-06-04 10:44:17 -0400

    Worked on removing and adding items to shopping cart

--- onlineTickets/Controllers/OrdersController.cs
+++ onlineTickets/Controllers/OrdersController.cs
@@ -29,5 +29,31 @@ namespace onlineTickets.Controllers
 
             return View(response);
         }
+
+        public async Task <IActionResult> AddItemToShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if(item != null)
+            {
+                _shoppingCart.AddItemToCart(item);
+
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+        }
+
+        public async Task<IActionResult> RemoveItemFromShoppingCart(int id)
+        {
+            var item = await _moviesService.GetMovieByIdAsync(id);
+
+            if (item != null)
+            {
+                _shoppingCart.RemoveItemFromCart(item);
+
+            }
+
+            return RedirectToAction(nameof(ShoppingCart));
+        }
     }
 }

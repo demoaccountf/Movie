ec3d160c2b8f6ee383f3105a0ef421a6efe8fb72
isomer04 <isomer008@gmail.com>
2023-06-05 00:08:07 -0400

    Added and Configured OrdersService

---
+++ onlineTickets/Data/ViewComponents/ShoppingCartSummary.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using onlineTickets.Data.Cart;
+
+namespace onlineTickets.Data.ViewComponents
+{
+    public class ShoppingCartSummary : ViewComponent
+    {
+
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+
+            return View(items.Count);
+        }
+    }
+}

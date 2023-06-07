731cfd5e511dd02fbaf95e0aa4acc45d3670ede9
isomer04 <isomer008@gmail.com>
2023-06-06 22:58:36 -0400

    Added Identity tables

--- onlineTickets/Migrations/20230607025557_Identity_Added.Designer.cs
+++ onlineTickets/Migrations/20230607025557_Identity_Added.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using onlineTickets.Data;
 
@@ -11,9 +12,11 @@ using onlineTickets.Data;
 namespace onlineTickets.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230607025557_Identity_Added")]
+    partial class Identity_Added
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ onlineTickets/Models/ApplicationUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace onlineTickets.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        [Display(Name = "Full name")]
+
+
+        public string FullName { get; set; }
+
+    }
+}

67fd00d2f4596d25c3bba6437ea642eb718b37fd
isomer04 <isomer008@gmail.com>
2023-06-07 22:54:42 -0400

    Added signing up users

--- onlineTickets/Controllers/AccountController.cs
+++ onlineTickets/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using onlineTickets.Data;
+using onlineTickets.Data.Static;
 using onlineTickets.Data.ViewModels;
 using onlineTickets.Models;
 
@@ -48,6 +49,31 @@ namespace onlineTickets.Controllers
 
         public IActionResult Register() => View(new RegisterVM());
 
+        [HttpPost]
 
+        public async Task<IActionResult> Register(RegisterVM registerVM)
+        {
+            if (!ModelState.IsValid) return View(registerVM);
+
+            var user = await _userManager.FindByEmailAsync(registerVM.EmailAddress);
+            if(user != null)
+            {
+                TempData["Error"] = "This email address is already in use";
+                return View(registerVM);
+            }
+
+            var newUser = new ApplicationUser()
+            {
+                FullName = registerVM.FullName,
+                Email = registerVM.EmailAddress,
+                UserName = registerVM.EmailAddress
+            };
+
+            var newUserResponse = await _userManager.CreateAsync(newUser, registerVM.Password);
+
+            if (newUserResponse.Succeeded) await _userManager.AddToRoleAsync(newUser, UserRoles.User);
+
+            return View("RegisterCompleted");
+        }
     }
 }
